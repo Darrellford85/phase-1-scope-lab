4e67ad5b80cda8a176b067b758c5442c38da41df
Darrell Ford <Darrellford85@gmail.com>
2022-03-26 20:07:57 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+// Learning slopes in js is a very slippery process//
+
+
+
+var customerName = "bob";
+
+//upperCaseCustomerName()//
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+// setBestCustomer()//
+function setBestCustomer() {
+   bestCustomer = "not bob" 
+}
+//overwriteBestCustomer()//
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+//  changeLeastFavoriteCustomer()//
+const LeastFavoriteCustomer = "Ana";
+
+function changeLeastFavoriteCustomer() {
+    LeastFavoriteCustomer = "Agatha";
+   
+}
+/* //Agatha is a crazy customer, call the cops//*/
