1e1ce026fca57c4ca2bdfe0f3f7b59f384ec5485
Darrell Ford <Darrellford85@gmail.com>
2022-03-26 19:14:43 -0700

    commit tree c446c7455478cab52971044e838bb7fe7a95aced

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+// Learning slopes in js is a very slippery process//
+
+let customerName = "bob";
+
+//upperCaseCustomerName()//
+upperCaseCustomerName() {
+    cusName = cusName.toUpperCase();
+    return cusName;
+}
